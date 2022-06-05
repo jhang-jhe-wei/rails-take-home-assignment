5f34749fe5a55df5841bc5d35f4b797888c9cde1
wells <jhang0912407249@gmail.com>
2022-06-06 01:08:16 +0800

    fix no user error in user_visits_landing_page_spec

--- spec/features/user_visits_landing_page_spec.rb
+++ spec/features/user_visits_landing_page_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 feature 'User visits landing page' do
   scenario 'they see an empty message and a create button' do
+    create(:user)
     visit root_path
 
     expect(page).to have_text("你還沒有建立股票清單哦！")
