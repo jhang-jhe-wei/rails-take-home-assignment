707629eca3da74719a38743abc178a3601daa380
wells <jhang0912407249@gmail.com>
2022-06-05 23:01:36 +0800

    Add landing page

---
+++ app/controllers/tracking_lists_controller.rb
@@ -0,0 +1,4 @@
+class TrackingListsController < ApplicationController
+  def index
+  end
+end
---
+++ spec/features/user_visits_landing_page_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature 'User visits landing page' do
+  scenario 'they see an empty message and a create button' do
+    visit root_path
+
+    expect(page).to have_text("你還沒有建立股票清單哦！")
+    expect(page).to have_button("點我建立新的追蹤清單")
+  end
+end
