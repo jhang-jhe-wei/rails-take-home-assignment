85f2e880af5b91c52f56b07b2fe9885d4332aac1
wells <jhang0912407249@gmail.com>
2022-06-06 15:34:23 +0800

    Add index spec in tracking list

--- spec/features/user_visits_landing_page_spec.rb
+++ spec/features/user_visits_landing_page_spec.rb
@@ -3,16 +3,29 @@
 require 'rails_helper'
 
 describe 'User visits landing page' do
-  before do
-    create(:user)
-    visit root_path
+  context 'When current user does not have tracking list' do
+    before do
+      create(:user)
+      visit root_path
+    end
+
+    it 'expects to see no tracking list message' do
+      expect(page).to have_text('你還沒有建立股票清單哦！')
+    end
+
+    it 'expects to see the create tracking list button' do
+      expect(page).to have_button('點我建立新的追蹤清單')
+    end
   end
 
-  it 'expects to see no tracking list message' do
-    expect(page).to have_text('你還沒有建立股票清單哦！')
-  end
-
-  it 'expects to see the create tracking list button' do
-    expect(page).to have_button('點我建立新的追蹤清單')
+  context 'When current user has many tracking list' do
+    let (:current_user) { create(:user) }
+    before do
+      create_list(:tracking_list, 3, user_id: current_user.id)
+      visit root_path
+    end
+    it 'expects to see their all tracking lists' do
+      expect(page).to have_text(/list\d/, count: 3)
+    end
   end
 end
