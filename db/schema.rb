012fb324f296479777353e6d515cb8ee3b44f14f
wells <jhang0912407249@gmail.com>
2022-06-06 21:52:20 +0800

    Add stock model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_06_040014) do
+ActiveRecord::Schema.define(version: 2022_06_06_135046) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tracking_lists", force: :cascade do |t|
     t.integer "user_id", null: false
