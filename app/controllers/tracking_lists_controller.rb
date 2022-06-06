81c3fd30d40cf4f1f79edd4500a9e02d01f63abe
wells <jhang0912407249@gmail.com>
2022-06-06 16:27:33 +0800

    Add update feature in tracking list

--- app/controllers/tracking_lists_controller.rb
+++ app/controllers/tracking_lists_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class TrackingListsController < ApplicationController
+  before_action :set_tracking_list, only: [:edit, :update]
+
   def index
     @tracking_lists = current_user.tracking_lists.all
   end
@@ -18,8 +20,23 @@ class TrackingListsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @tracking_list.update(tracking_list_params)
+      redirect_to tracking_lists_url, notice: '追蹤清單已成功修改'
+    else
+      render :edit
+    end
+  end
+
   private
   def tracking_list_params
     params.require(:tracking_list).permit(:name)
+  end
+
+  def set_tracking_list
+    @tracking_list = current_user.tracking_lists.find(params[:id])
   end
 end
---
+++ spec/features/user_edits_the_tracking_list_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'User edits the tracking list' do
+  context 'When current user has one tracking list' do
+    let (:current_user) { create(:user) }
+    before do
+      create(:tracking_list, user_id: current_user.id)
+      visit root_path
+      click_link("編輯")
+      fill_in 'Name', with: "Updated List"
+      click_button('Update Tracking list')
+    end
+
+    it 'expects to see name of the tracking_list has changed' do
+      expect(page).to have_text("Updated List")
+    end
+
+    it 'expects to see the updated successfully message' do
+      expect(page).to have_text("追蹤清單已成功修改")
+    end
+  end
+
+  context 'When current user has many tracking lists' do
+    let (:current_user) { create(:user) }
+    before do
+      create_list(:tracking_list, 3, user_id: current_user.id)
+      visit root_path
+      second_tracking_list.click_link('編輯')
+      fill_in 'Name', with: "Updated List"
+      click_button('Update Tracking list')
+    end
+
+    it 'expects to see name of the second tracking list has changed' do
+      expect(second_tracking_list).to have_text('Updated List')
+    end
+
+    def second_tracking_list
+      all('div.tracking-list')[1]
+    end
+  end
+end
